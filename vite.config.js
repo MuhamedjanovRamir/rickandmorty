6502810984ade83ccb11e2450f60ed2a6290a011
MuhamedjanovRamir <muhamedjanovramir@gmail.com>
2024-06-11 22:23:12 +0500

    2

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vitePluginGhPages from 'vite-plugin-gh-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +10,6 @@ export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
-    vitePluginGhPages()
   ],
   resolve: {
     alias: {
